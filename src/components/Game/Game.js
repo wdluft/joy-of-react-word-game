2f3d406eeefa06c0e33346e36b8155f6f168cc08
Will Luft <william.d.luft@gmail.com>
2024-01-30 08:28:14 -0500

    Created PreviousGuesses component to show all guesses. Updated GuessInput to use the pattern attribute for validation.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
+import PreviousGuesses from '../PreviousGuesses/PreviousGuesses';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,9 +11,16 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+  function addGuess(guess) {
+    const newGuess = { guess, id: Math.random() };
+    setGuesses([...guesses, newGuess]);
+  }
+
   return (
     <>
-      <GuessInput />
+      <PreviousGuesses guesses={guesses} />
+      <GuessInput addGuess={addGuess} />
     </>
   );
 }
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
-function GuessInput() {
+function GuessInput({ addGuess }) {
   const [guess, setGuess] = React.useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log(guess);
+    addGuess(guess);
     setGuess('');
   }
 
   return (
-    <form class='guess-input-wrapper' onSubmit={handleSubmit}>
-      <label for='guess-input'>Enter guess:</label>
+    <form className='guess-input-wrapper' onSubmit={handleSubmit}>
+      <label htmlFor='guess-input'>Enter guess:</label>
       <input
         id='guess-input'
         type='text'
         value={guess}
         onChange={(event) => setGuess(event.target.value.toUpperCase())}
         maxLength={5}
-        minLength={5}
+        // minLength={5}
+        pattern='[a-zA-Z]{5}'
       />
     </form>
   );
